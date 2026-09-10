2a0c14a29d53e9c2c7a7b3e588bd777ad2447611
Batidão-OozmaKappa-Coder <ozzmakappa89@gmail.com>
2026-09-10 18:48:27 -0300

    Change greeting from 'Hello World' to 'Goodbye World'

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* script.js */
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBhvhhuu3AAQuRNbElpqOyE3-I00tU1Uw",
+  apiKey: "AIzaSyBhvhhuu3AAQuRNbElpqOyE3-I00tU1UJw",
   authDomain: "sistema-casa-de-sucos.firebaseapp.com",
   databaseURL: "https://sistema-casa-de-sucos-default-rtdb.firebaseio.com",
   projectId: "sistema-casa-de-sucos",
